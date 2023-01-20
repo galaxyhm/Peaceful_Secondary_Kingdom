3cf191d5f65d91297e33fe61e65b4e9dc2a59edc
galaxyhm <galaxyeunha0530@gmail.com>
2023-01-20 14:11:13 +0900

    make models and some of view

--- board/models.py
+++ board/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    # 거래코드
+    TRANSACTION_CODE = (
+        (1, '직거래'),
+        (2, '안전거래'),
+        (3, '직거래 + 안전거래'),
+
+    )
+    # user = models.ForeignKey() Account 개발 될때 까지 중단
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    transaction_code = models.PositiveSmallIntegerField(choices=TRANSACTION_CODE, default=1)
+    is_expired = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modify_at = models.DateTimeField(auto_now=True)
+    price = models.PositiveIntegerField()
+
+
+class Photo(models.Model):
+    # user = models.ForeignKey
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='photos')
+    image = models.ImageField(upload_to='images/')
+
+
+class Comment(models.Model) :
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    add_modify_at = models.DateTimeField(auto_now=True)
+
--- peaceful_secondary_kingdom/settings.py
+++ peaceful_secondary_kingdom/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 미디어 파일을 관리할 루트 media 디렉터리
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# 각 media file에 대한 URL prefix
+MEDIA_URL = '/media/'
